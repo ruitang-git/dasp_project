12af0df1432d2710a68fe0902daf983eb00c8ec9
ruitang-git <82528575+ruitang-git@users.noreply.github.com>
2021-05-29 14:27:02 +0200

    Add files via upload

---
+++ mmse_based_noise_psd.m
@@ -0,0 +1,39 @@
+%% MMSE
+function sigma_2 = mmse_based_noise_psd(x,fs,time_int)
+% segamentation
+N = length(x);
+N_seg = time_int * fs;
+seg = ceil(N/N_seg); % number of segamentations
+zeropad = seg*N_seg -N;
+x_pad = [x',zeros(1,zeropad)];
+x_seg = reshape(x_pad,[N_seg,seg]); % one column = one segamentation
+% fft
+f = [];
+for j = 1:seg
+    f(:,j) = fft(x_seg(:,1));
+end 
+% psd estimation
+p_H0 = 0.5;p_H1 = 0.5;
+alpha = 0.8;
+noise_psd = [];
+sigma_2 = [];
+cosin = 10^1.5;
+for j = 1:5
+    for k = 1:N_seg
+       noise_psd(k,j) = f(k,j)*f(k,j)';
+       if j ~= 1
+           sigma_2(k,j) = noise_psd(k,j);
+       else
+           sigma_2(k,j) = alpha*sigma_2(k,j-1)+(1-alpha)*noise_psd(k,j);   
+       end
+    end
+end
+for j = 6:seg
+    for k = 1:N_seg
+        p_H1y = (1+p_H0/p_H1*(1+cosin)*exp(-(f(k,j)*f(k,j)'*cosin)/((1+cosin)*sigma_2(k,j-1))));
+        p_H0y = 1-p_H1y;
+        noise_psd(k,j) = p_H0y*f(k,j)*f(k,j)'+ p_H1y*sigma_2(k,j-1);
+        sigma_2(k,j) = alpha*sigma_2(k,j-1)+(1-alpha)*noise_psd(k,j);
+    end
+end
+sigma_2 = sigma_2./N_seg;
---
+++ speech_enhancement.m
@@ -0,0 +1,22 @@
+%% single channel
+[bn,fs] = audioread("D:\slides\EE4182 Digital Audio and Speech Processing\project\sound files for mini-project\babble_noise.wav");
+[ssn,~] = audioread("D:\slides\EE4182 Digital Audio and Speech Processing\project\sound files for mini-project\Speech_shaped_noise.wav");
+[cs2,~] = audioread("D:\slides\EE4182 Digital Audio and Speech Processing\project\sound files for mini-project\clean_speech_2.wav");
+[cs,~] = audioread("D:\slides\EE4182 Digital Audio and Speech Processing\project\sound files for mini-project\clean_speech.wav");
+[ann,~] = audioread("D:\slides\EE4182 Digital Audio and Speech Processing\project\sound files for mini-project\aritificial_nonstat_noise.wav");
+seconds=10; range=fs*10+1:fs*(10+seconds);
+bn=bn(range); ssn=ssn(range); cs2=cs2(range);cs=cs(range); ann=ann(range);
+cs_bn = cs+bn;
+cs_ssn = cs+ssn;
+cs_ann = cs+ann;
+cs2_bn = cs2+bn;
+cs2_ssn = cs2+ssn;
+cs2_ann = cs2+ann;
+%% gain function
+%% noise psd estimation
+% VAD
+% MMSE
+time_int = 0.02;% second
+noise_psd = mmse_based_noise_psd(x,fs,time_int);
+%% signal psd estimation
+%% multiple channel
